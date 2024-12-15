9c8755ce3099f7e3d555d3520f8225a5620fd26e
[PDhru] <[dhrutip2005@gmail.com]>
2024-12-16 00:41:02 +0530

    chnge in multer to add webp img

--- backend/middlewares/multerConfig.js
+++ backend/middlewares/multerConfig.js
@@ -1,31 +1,33 @@
-const multer = require('multer');
-const path = require('path');
-
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads/'); 
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`); 
-    }
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
-    if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(new Error('Only JPEG, JPG, and PNG file types are allowed!'), false);
-    }
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true); // File type is allowed
+  } else {
+    cb(
+      new Error("Only JPEG, JPG, PNG, and WEBP file types are allowed!"),
+      false
+    ); // Reject file
+  }
 };
 
 // Multer upload config
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1024 * 1024 * 5 },
-    fileFilter: fileFilter
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;
